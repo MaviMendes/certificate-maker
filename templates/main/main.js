4417722495deab2147459d116b74e26e96be904a
Bruno Gabriel <brunogabriel.justino@usp.br>
2023-06-28 02:19:06 -0300

    front-end fineshed

---
+++ templates/main/main.js
@@ -0,0 +1,9 @@
+const modal = document.getElementById('modalElement')
+
+function closeModal() { 
+    modal.style.display = "none"
+}
+
+function openModal(){
+    modal.style.display = "flex"    
+}
